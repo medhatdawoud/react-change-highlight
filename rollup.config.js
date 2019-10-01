c301253212f12eb7769941afcf94077fd3044e06
Medhat Dawoud <medhatdawoud@gmail.com>
2019-10-02 01:25:00 +0200

    Add minification

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
+import { uglify } from "rollup-plugin-uglify";
 
 import packageJSON from "./package.json";
 const input = "./src/index.js";
+const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
 
 export default [
   // CommonJS
@@ -21,6 +23,22 @@ export default [
       external(),
       resolve(),
       commonjs()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.main),
+      format: "cjs"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      uglify()
     ]
   }
 ];
