6f3e9905960c822c39b4874db3554b73781f78e4
Medhat Dawoud <medhatdawoud@gmail.com>
2019-12-15 21:51:00 +0100

    Remove comment and change var to const

--- src/Util/addStyleString.ts
+++ src/Util/addStyleString.ts
@@ -1,12 +1,12 @@
+/* eslint-disable no-console */
 const addStyleString = (str: string, isSSR = false) => {
-  // do nothing in case of SSR
   if (!document || isSSR) {
     console.warn(
       `"react-change-highlight", As you're using server-side rendering, it's a must to create and pass your css class to the highlightStyle attribute for "ChangeHighlight" component.`
     );
     return;
   }
-  var node = document.createElement("style");
+  const node = document.createElement("style");
   node.innerHTML = str;
   document.body.appendChild(node);
 };
