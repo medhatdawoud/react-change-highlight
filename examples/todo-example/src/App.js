5ee6c5cf76528acb95d6b9d45d3ac5de931eaf3e
Medhat Dawoud <medhatdawoud@gmail.com>
2020-01-29 00:40:35 +0100

    Add todo logic to the todo example

--- examples/todo-example/src/App.js
+++ examples/todo-example/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
-import "./App.css";
+import React, { useRef, useState } from "react";
+import HighlightChange from "react-change-highlight";
 
 function App() {
+  const inputRef = useRef();
+  const [listOfTodos, setListOfTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (!inputRef.current.value) return;
+    const list = [...listOfTodos];
+    list.push(inputRef.current.value);
+    setListOfTodos(list);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      todo app
+    <div style={{ margin: "50px 100px" }}>
+      <input
+        ref={inputRef}
+        onKeyDown={e => {
+          if (e.keyCode === 13) handleAddTodo();
+        }}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <br />
+      <HighlightChange>
+        {listOfTodos.map((todo, idx) => (
+          <div key={todo + idx} style={{ paddingTop: "10px" }} ref={React.createRef()}>
+            <input type="checkbox" />
+            {todo}
+          </div>
+        ))}
+      </HighlightChange>
     </div>
   );
 }
