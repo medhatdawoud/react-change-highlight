98eea2fa96f92de5a92ae88b4ab72ecc337478c5
Medhat Dawoud <MedhatDawoud@gmail.com>
2020-01-30 00:23:56 +0100

    Example/Todo example (#30)
    * Adding initial app
    
    * Add todo logic to the todo example
    
    * Fix yarn conflict
    
    * Added todo example for the newOnly mode

---
+++ examples/todo-example/src/App.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import HighlightChange from "react-change-highlight";
+
+function App() {
+  const inputRef = useRef();
+  const [listOfTodos, setListOfTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (!inputRef.current.value) return;
+    const list = [...listOfTodos];
+    list.push(inputRef.current.value);
+    setListOfTodos(list);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
+  return (
+    <div style={{ margin: "50px 100px" }}>
+      <input
+        ref={inputRef}
+        onKeyDown={e => {
+          if (e.keyCode === 13) handleAddTodo();
+        }}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <br />
+      <HighlightChange highlightStyle="highlight" mode="newOnly">
+        {listOfTodos.map((todo, idx) => (
+          <div key={todo + idx} style={{ padding: "10px 0" }} ref={React.createRef()}>
+            <input type="checkbox" />
+            {todo}
+          </div>
+        ))}
+      </HighlightChange>
+    </div>
+  );
+}
+
+export default App;
